79122f67d950e8cdb8d8d522390d41f8cbce348b
Mario Fraire <fraire.tepox.mario.valentin@gmail.com>
2025-07-22 19:29:24 -0600

    analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import "./main.css";
 import App from "./App.tsx";
 import { AppRouter } from "./RouterDom.tsx";
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <App>
+        <Analytics />
         <AppRouter />
       </App>
     </BrowserRouter>
